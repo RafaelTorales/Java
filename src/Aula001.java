24a839115d6fff56574019bc3fe77a4d22722b30
Rafael <rdz@certi.org.br>
2024-04-29 20:15:42 -0300

    feat: trabalhando com metodos

--- src/Aula001.java
+++ src/Aula001.java
@@ -1,5 +1,14 @@
 public class Aula001 {
     public static void main(String[] args) {
-        System.out.println("Eai");
+        final String BR = "Brasil"; // Const
+        String primeiroNome = "Rafael";
+        String segundoNome = "Torales";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
