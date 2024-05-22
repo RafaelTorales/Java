5cbdb35c5c824d9843fe98e8dbd380a5c47f6604
Rafael <rdz@certi.org.br>
2024-05-22 16:44:34 -0300

    Java Doc

---
+++ src/aula/calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+package aula.calculadora;
+
+/**
+ * <Calculadora</h1>
+ * <p>
+ * A Calculadora realiza operações matemática entre números inteiros <b>Note:</b> Leia atentamente a
+ * documentação desta classe para desfrutar dos recursos oferecidos pelo autor
+ * </p>
+ * 
+ * @author Rafael Torales
+ * @version 1.0
+ * @since 22/05/2024
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * 
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+
+}
